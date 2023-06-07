1da663aa2d6fd5f098a1cc8fc7155e139b9dd6c4
Nicolas Zozor <nick.zozor@gmail.com>
2023-06-07 16:08:38 +0100

    update metas

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -25,8 +25,13 @@ export default function App({ Component, pageProps }: AppProps) {
           <Head>
               <title>A BODY FOREVER | 90 DAY ELEVATED PROGRAMME</title>
               <link rel="shortcut icon" href="/cropped-favicon-2-32x32.png" />
-              <meta name="facebook-domain-verification” content=“m82mr5nth1zt0ge4u7ppcocl3fyn0i" />
+              <meta name="facebook-domain-verification" content="m82mr5nth1zt0ge4u7ppcocl3fyn0i" />
               <meta property="og:image" content="https://www.abodyforeverprogramme.com/_next/image?url=%2FABF_Font-Page-Image-02.png&w=3840&q=75" />
+              <meta property="og:url" content="https://www.abodyforeverprogramme.com/" />
+              <meta property="og:title" content="A BODY FOREVER | 90 DAY ELEVATED PROGRAMME" />
+              <meta property="og:description" content="Are you a busy woman looking to feel happy, healthy, and more confident in both your body and mind? Look no further than A Body Forever - the online wellness platform designed for you! Our simple and effective program can be done anywhere, anytime without completely disrupting your life." />
+              <meta property="og:image:alt" content="A Body Forever Logo" />
+
           </Head>
         <style jsx global>{`
         :root {
